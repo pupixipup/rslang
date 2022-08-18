1cbf08947ca10a500b14903035dd8d4bc4895366
pupixipup <rkbitinas@gmail.com>
2022-08-18 10:42:26 +0200

    fix: prevent multiple similar audios playing at once

--- src/textbook/WordAudio.tsx
+++ src/textbook/WordAudio.tsx
@@ -1,18 +1,25 @@
-import React, { useState, useEffect, MouseEventHandler } from 'react'
-// import { IWord, API } from './api';
+import React, { useState } from 'react'
 import playAudios from './playAudios';
+
 interface linkProps {
   audioLink: string | undefined
   audioMeaningLink: string | undefined
   audioExampleLink: string | undefined
 }
 
+
 function WordAudio(props: linkProps) {
+  const [isPlaying, setPlaying] = useState(false);
   const { audioLink , audioMeaningLink, audioExampleLink } = props;
   const sounds = [audioLink, audioMeaningLink, audioExampleLink]; 
   return (
     <div>
-      <button onClick={() => {playAudios(sounds)}}> Play </button>
+      <button onClick={() => {
+        if (isPlaying === false) {
+          setPlaying(!isPlaying); 
+          playAudios(sounds, setPlaying);
+        }
+        }}> Play </button>
   </div>
   )
 }
--- src/textbook/playAudios.ts
+++ src/textbook/playAudios.ts
@@ -1,15 +1,20 @@
 import { API } from './api';
-const playAudios = (sounds: (string | undefined)[]) => {
+const playAudios = (sounds: (string | undefined)[], setState: React.Dispatch<React.SetStateAction<boolean>>) => {
   const api = new API();
   let index = 0;
   let audio = new Audio();
   audio.src = `${api.baseUrl}/${sounds[index]}`;
   audio.play();
-  if (index < 3) {
-  audio.onended = () => { 
-    audio.src = `${api.baseUrl}/${sounds[index += 1]}`;
+    audio.onended = () => { 
+    index += 1;
+    if (index < sounds.length) {
+    audio.src = `${api.baseUrl}/${sounds[index]}`;
     audio.play();
-  };
+  } else {
+    audio.pause();
+    setState(false);
+    audio.currentTime = 0;
+  }
 }
 }
 export default playAudios
