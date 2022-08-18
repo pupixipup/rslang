14928682ad7ebb784a55ba582baf4cff676efe29
pupixipup <rkbitinas@gmail.com>
2022-08-18 15:28:26 +0200

    feat: add switching between sections

--- src/textbook/Textbook.tsx
+++ src/textbook/Textbook.tsx
@@ -1,23 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import {IWord, API } from './api';
 import Card from './Card';
+import './Textbook.css';
 
 function Textbook() {
+  const [numbers, setNumbers] = useState([1, 2]);
   const [data, updateData] = useState<IWord[]>();
   useEffect(() => {
+    console.log(numbers);
     const api = new API();
-    api.getWords(1, 2).then((words) => {
+    api.getWords(numbers[0], numbers[1]).then((words) => {
       updateData(words);
     });
-  }, []);
+  }, numbers);
   return (
-    <div>
+    <div className="textbook">
+    <div className="words">
       {
         data?.map((word, ndx) => {
           return <Card word={word} key={ndx}/>
         })
       } 
      </div>
+     <div>
+      <div className="sections">
+        <button onClick={() => {setNumbers([1, 0])}} className="sections__section">1</button>
+        <button onClick={() => {setNumbers([1, 1])}} className="sections__section">2</button>
+        <button onClick={() => {setNumbers([1, 2])}} className="sections__section">3</button>
+        <button onClick={() => {setNumbers([1, 3])}} className="sections__section">4</button>
+        <button onClick={() => {setNumbers([1, 4])}} className="sections__section">5</button>
+        <button onClick={() => {setNumbers([1, 5])}} className="sections__section">6</button>
+      </div>
+     </div>
+    </div>
   )
 }
 
