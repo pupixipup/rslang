d26e7733f608bc3a9c9853a4473119fe32539c0c
pupixipup <rkbitinas@gmail.com>
2022-08-18 15:48:38 +0200

    feat: add saving current page location in the storage

--- src/textbook/Textbook.tsx
+++ src/textbook/Textbook.tsx
@@ -4,15 +4,21 @@ import Card from './Card';
 import './Textbook.css';
 
 function Textbook() {
-  const [numbers, setNumbers] = useState([1, 2]);
+  const wordsLocation = JSON.parse(window.localStorage.getItem('wordsLocation') as string) || [0, 0];
+  const [numbers, setNumbers] = useState(wordsLocation);
   const [data, updateData] = useState<IWord[]>();
   useEffect(() => {
-    console.log(numbers);
     const api = new API();
     api.getWords(numbers[0], numbers[1]).then((words) => {
       updateData(words);
     });
-  }, numbers);
+  }, [numbers]);
+
+  function setPage(arr: number[]) {
+    window.localStorage.setItem('wordsLocation', JSON.stringify(arr));
+    setNumbers(arr);
+  }
+
   return (
     <div className="textbook">
     <div className="words">
@@ -24,12 +30,12 @@ function Textbook() {
      </div>
      <div>
       <div className="sections">
-        <button onClick={() => {setNumbers([1, 0])}} className="sections__section">1</button>
-        <button onClick={() => {setNumbers([1, 1])}} className="sections__section">2</button>
-        <button onClick={() => {setNumbers([1, 2])}} className="sections__section">3</button>
-        <button onClick={() => {setNumbers([1, 3])}} className="sections__section">4</button>
-        <button onClick={() => {setNumbers([1, 4])}} className="sections__section">5</button>
-        <button onClick={() => {setNumbers([1, 5])}} className="sections__section">6</button>
+        <button onClick={() => {setPage([1, 0])}} className="sections__section">1</button>
+        <button onClick={() => {setPage([1, 1])}} className="sections__section">2</button>
+        <button onClick={() => {setPage([1, 2])}} className="sections__section">3</button>
+        <button onClick={() => {setPage([1, 3])}} className="sections__section">4</button>
+        <button onClick={() => {setPage([1, 4])}} className="sections__section">5</button>
+        <button onClick={() => {setPage([1, 5])}} className="sections__section">6</button>
       </div>
      </div>
     </div>
