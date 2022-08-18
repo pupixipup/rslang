1d12fa31a0e3cbade5dba8d9f73612c9da7c77a8
pupixipup <rkbitinas@gmail.com>
2022-08-19 00:50:58 +0200

    feat: add pagination

--- src/textbook/Textbook.tsx
+++ src/textbook/Textbook.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {IWord, API } from './api';
+import Pagination from './Pagination';
+import ReactPaginate from 'react-paginate'
 import Card from './Card';
 import './Textbook.css';
 
@@ -7,6 +9,7 @@ function Textbook() {
   const wordsLocation = JSON.parse(window.localStorage.getItem('wordsLocation') as string) || [0, 0];
   const [numbers, setNumbers] = useState(wordsLocation);
   const [data, updateData] = useState<IWord[]>();
+
   useEffect(() => {
     const api = new API();
     api.getWords(numbers[0], numbers[1]).then((words) => {
@@ -17,18 +20,19 @@ function Textbook() {
   function setPage(arr: number[]) {
     window.localStorage.setItem('wordsLocation', JSON.stringify(arr));
     setNumbers(arr);
+    console.log(JSON.stringify(arr));
   }
 
   return (
+    <div>
     <div className="textbook">
     <div className="words">
       {
         data?.map((word, ndx) => {
           return <Card word={word} key={ndx}/>
         })
-      } 
+      }
      </div>
-     <div>
       <div className="sections">
         <button onClick={() => {setPage([1, 0])}} className="sections__section">1</button>
         <button onClick={() => {setPage([1, 1])}} className="sections__section">2</button>
@@ -38,7 +42,18 @@ function Textbook() {
         <button onClick={() => {setPage([1, 5])}} className="sections__section">6</button>
       </div>
      </div>
-    </div>
+     <ReactPaginate
+      className="pagination"
+      previousLabel={"<"}
+      nextLabel={">"}
+      pageCount={30}
+      onPageChange={ ({selected}) => {
+        setPage([
+          selected,
+          JSON.parse(window.localStorage.getItem('wordsLocation') as string)[1]]);
+      }}
+      />
+  </div>
   )
 }
 
