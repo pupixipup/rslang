d41173f390e40d3dce93cd6b67eb507823f22963
pupixipup <rkbitinas@gmail.com>
2022-08-19 12:10:21 +0200

    refactor: move sections to a separated module

--- src/textbook/Textbook.tsx
+++ src/textbook/Textbook.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import {createSectionsArray, Section} from './Section';
 import {IWord, API } from './api';
-import Pagination from './Pagination';
 import ReactPaginate from 'react-paginate'
 import Card from './Card';
 import './Textbook.css';
@@ -9,6 +9,9 @@ function Textbook() {
   const wordsLocation = JSON.parse(window.localStorage.getItem('wordsLocation') as string) || [0, 0];
   const [numbers, setNumbers] = useState(wordsLocation);
   const [data, updateData] = useState<IWord[]>();
+
+  const totalSections = 5;
+  const sectionsArray: number[] = createSectionsArray(totalSections);
 
   useEffect(() => {
     const api = new API();
@@ -20,8 +23,8 @@ function Textbook() {
   function setPage(arr: number[]) {
     window.localStorage.setItem('wordsLocation', JSON.stringify(arr));
     setNumbers(arr);
-    console.log(JSON.stringify(arr));
   }
+
 
   return (
     <div>
@@ -34,12 +37,11 @@ function Textbook() {
       }
      </div>
       <div className="sections">
-        <button onClick={() => {setPage([1, 0])}} className="sections__section">1</button>
-        <button onClick={() => {setPage([1, 1])}} className="sections__section">2</button>
-        <button onClick={() => {setPage([1, 2])}} className="sections__section">3</button>
-        <button onClick={() => {setPage([1, 3])}} className="sections__section">4</button>
-        <button onClick={() => {setPage([1, 4])}} className="sections__section">5</button>
-        <button onClick={() => {setPage([1, 5])}} className="sections__section">6</button>
+        {
+      sectionsArray.map((number) => {
+        return <Section location={numbers} key={number} sectionId={number} setPage={setPage} />
+      })
+      }
       </div>
      </div>
      <ReactPaginate
@@ -47,6 +49,7 @@ function Textbook() {
       previousLabel={"<"}
       nextLabel={">"}
       pageCount={30}
+      forcePage={numbers[0]}
       onPageChange={ ({selected}) => {
         setPage([
           selected,
