5f275a917eec790caf95631341a381c9df846108
pupixipup <rkbitinas@gmail.com>
2022-08-19 14:40:44 +0200

    refactor: use destructuring

--- src/components/textbook/Card.tsx
+++ src/components/textbook/Card.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import './Card.scss';
 import WordAudio from './WordAudio';
-import { IWord, API } from './api';
+import { IWord } from './api';
 interface wordProps {
-  word: IWord | undefined
+  word: IWord | undefined,
+  link: string
 }
 function Card(props: wordProps) {
-  const api = new API();
+  const { word, link } = props;
   return (
     <div className="word">
       <div className="word__word">
-        <div className="word__word-name">{props.word?.word}</div>
-        <div className="word__word-transcription">{props.word?.transcription}</div>
-        <div className="word__word-translation">{props.word?.wordTranslate}</div>
+        <div className="word__word-name">{word?.word}</div>
+        <div className="word__word-transcription">{word?.transcription}</div>
+        <div className="word__word-translation">{word?.wordTranslate}</div>
         <WordAudio 
-        audioLink={props.word?.audio} 
-        audioMeaningLink={props.word?.audioMeaning} 
-        audioExampleLink={props.word?.audioExample}
+        audioLink={word?.audio} 
+        audioMeaningLink={word?.audioMeaning} 
+        audioExampleLink={word?.audioExample}
         />
       </div>
       <div className="word__meaning">
-        <div className="word__meaning-eng">{props.word?.textMeaning}</div>
-        <div className="word__meaning-ru">{props.word?.textMeaningTranslate}</div>
+        <div className="word__meaning-eng">{word?.textMeaning}</div>
+        <div className="word__meaning-ru">{word?.textMeaningTranslate}</div>
       </div>
       <div className="word__example">
-        <div className="word__example-eng">{props.word?.textExample}</div>
-        <div className="word__example-ru">{props.word?.textExampleTranslate}</div>
+        <div className="word__example-eng">{word?.textExample}</div>
+        <div className="word__example-ru">{word?.textExampleTranslate}</div>
       </div>
-      <img src={`${api.baseUrl}/${props.word?.image}`} alt={props.word?.word} />
+      <img src={link} alt={word?.word} />
     </div>
   )
 }
