fb2d8a1ff5ce0dd930c3af46d5af10bcd67765a2
pupixipup <rkbitinas@gmail.com>
2022-08-19 13:43:20 +0200

    fix: IWord interface types

--- src/components/textbook/api.ts
+++ src/components/textbook/api.ts
@@ -1,18 +1,18 @@
 export interface IWord {
-  "id": "string",
-  "group": 0,
-  "page": 0,
-  "word": "string",
-  "image": "string",
-  "audio": "string",
-  "audioMeaning": "string",
-  "audioExample": "string",
-  "textMeaning": "string",
-  "textExample": "string",
-  "transcription": "string",
-  "wordTranslate": "string",
-  "textMeaningTranslate": "string",
-  "textExampleTranslate": "string"
+  id: string,
+  group: number,
+  page: number,
+  word: string,
+  image: string,
+  audio: string,
+  audioMeaning: string,
+  audioExample: string,
+  textMeaning: string,
+  textExample: string,
+  transcription: string,
+  wordTranslate: string,
+  textMeaningTranslate: string,
+  textExampleTranslate: string
 }
 
 export class API {
