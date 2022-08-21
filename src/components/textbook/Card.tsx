1bb3e34f67d2050ec13fdded284377ed63735a56
pupixipup <rkbitinas@gmail.com>
2022-08-22 00:02:05 +0200

    feat: restyle textbook

--- src/components/textbook/Card.tsx
+++ src/components/textbook/Card.tsx
@@ -11,6 +11,7 @@ function Card(props: wordProps) {
   const { word, link } = props;
   return (
     <div className="word">
+        <div className="word__desc">
       <div className="word__word">
         <div className="word__word-name">{word?.word}</div>
         <div className="word__word-transcription">{word?.transcription}</div>
@@ -29,6 +30,7 @@ function Card(props: wordProps) {
         <div className="word__example-eng" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(word?.textExample as string)}} />
         <div className="word__example-ru">{word?.textExampleTranslate}</div>
       </div>
+        </div>
       <img className="word__image" src={link} alt={word?.word} />
     </div>
   )
