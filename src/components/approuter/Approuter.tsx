6345f96e2415e0efaa1fe6d9adf18e1d7a6387f6
vpuzyrevich <v.puzyrevich@gmail.com>
2022-08-24 13:15:48 +0300

    feat: add games page

--- src/components/approuter/Approuter.tsx
+++ src/components/approuter/Approuter.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Auth } from "../auth/Auth";
+import { Games } from "../games/Games";
+import { Sprint } from "../games/sprint/Sprint";
 import Home from "../home/Home";
 import Textbook from "../textbook/Textbook";
 import "./Approuter.scss";
@@ -18,6 +20,20 @@ const MENUITEMS =[
         value: 'TEXTBOOK',
         link: 'textbook'
     },
+    {
+      value: 'Игры',
+      link: 'games'
+  },
+]
+const ROUTEITEMS = [
+  {
+    value: 'Sprint',
+    link: 'sprint'
+  },
+  {
+    value: 'Audio',
+    link: 'audio'
+  },
 ]
 
   
@@ -37,6 +53,8 @@ function AppRouter() {
               <Route path="/" element ={<Home />}/>
               <Route path={'/' + MENUITEMS[1].link} element ={<Auth />}/>
               <Route path={'/' + MENUITEMS[2].link} element ={<Textbook />}/>
+              <Route path={'/' + MENUITEMS[3].link} element ={<Games />}/>
+              <Route path={'/' + MENUITEMS[3].link + '/' + ROUTEITEMS[0].link} element ={<Sprint />}/>
             </Routes>
           </section>
           </BrowserRouter>
---
+++ src/components/games/Games.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import './Games.scss';
+
+export function Games () {
+  let navigate = useNavigate();
+  return(
+    <div className='games-wrapper'>
+      <div className='game game-sprint' onClick={() => navigate('/games/sprint', { replace: true })}>
+        <div className='game-title'>Спринт</div>
+      </div>
+      <div className='game game-audio'>
+        <div className='game-title'>Аудиовызов</div>
+      </div>
+    </div>
+  )
+}
