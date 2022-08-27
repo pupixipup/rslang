ac80f8cd568901ad278aa41264d9f785b9301af1
pupixipup <rkbitinas@gmail.com>
2022-08-27 10:25:08 +0200

    fix: prevent erasing old data on userword update

--- src/components/textbook/wordapi.ts
+++ src/components/textbook/wordapi.ts
@@ -6,17 +6,16 @@ export async function sendLocalWords(localWordsList: localWord[]) {
   let requests: Promise<void>[] = [];
   console.log(localWordsList);
   localWordsList.forEach((word) => {
+    console.log(word);  
     requests.push(setWord(word._id, word.userWord));
   });
   await Promise.allSettled(requests);
   localWordsList = [];
 }
 
-export async function setWord(id: string, optional: IUserWordOptions) {
-  API.createUserWord(id, { difficulty: 'hard',  })
+export async function setWord(id: string, userWord: IUserWordOptions) {
+  API.createUserWord(id, { difficulty: userWord.difficulty})
       .catch((err) => {
-          API.updateUserWord(id, { difficulty: 'hard', optional: {
-              learnt: false
-          }})
+          API.updateUserWord(id, userWord)
       })
 }
