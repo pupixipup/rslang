6f1da24e79677bf3664948eaf298f87f46aef436
pupixipup <rkbitinas@gmail.com>
2022-08-27 14:21:18 +0200

    feat: add instant remove from hard words
    when 'unmark' button is clicked, the card is removed instantly

--- src/components/textbook/Card.tsx
+++ src/components/textbook/Card.tsx
@@ -1,59 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import DOMPurify from 'dompurify';
-import { sendLocalWords } from "./wordapi";
-import './Card.scss';
-import WordAudio from './WordAudio';
-import { IUserWord, IWord, localWord, wordsList, IPagenatedResult } from "../../common/interfaces";
+import React, { useEffect, useState } from "react";
+import DOMPurify from "dompurify";
+import { createLocalWord } from "./wordapi";
+import { areObjectsEqual } from "./utils";
+import "./Card.scss";
+import WordAudio from "./WordAudio";
+import {
+  IUserWord,
+  IWord,
+  localWord,
+  wordsList,
+  IPagenatedResult,
+} from "../../common/interfaces";
 
 interface wordProps {
-  wordsArray: wordsList,
-  word: IUserWord | IWord,
-  localWords: Array<localWord>,
-  updateLocalWords: (arr: localWord[]) => void,
-  link: string,
-  updateWords: (arr: IUserWord[]) => void,
-  numbers: { page: number, section: number },
-  isLoggedIn: boolean
+  wordsArray: wordsList;
+  word: IUserWord | IWord;
+  localWords: Array<localWord>;
+  updateLocalWords: (arr: localWord[]) => void;
+  link: string;
+  updateWords: (arr: IUserWord[]) => void;
+  numbers: { page: number; section: number };
+  isLoggedIn: boolean;
 }
 
-
 function Card(props: wordProps) {
-  const { word, link, isLoggedIn, numbers, localWords, updateLocalWords } = props;
+  const { word, link, isLoggedIn, numbers, localWords, updateLocalWords, wordsArray, updateWords } =
+    props;
   let buttons = <div></div>;
-  
+
   const [difficultWord, setDifficultWord] = useState(word);
 
-
   if (isLoggedIn) {
-    buttons = <div className="word__interact">
-      <button
-        className='words__interact-hard'
-        onClick={() => {
-          const newDifficulty = (difficultWord as IUserWord).userWord?.difficulty === 'hard' ? 'easy' : 'hard'
-          const newIsLearnt = newDifficulty === 'hard' ? false : true;
-
-          const newLocalWord = { _id: (word as IPagenatedResult)._id, userWord: {
-             difficulty: newDifficulty, optional: { ...(word as IUserWord).userWord?.optional, learnt: newIsLearnt }
-             }, isUserWord: !!(word as IUserWord).userWord }
-
-             let ids = localWords.map((element) => element._id);
-            if (!ids.includes(newLocalWord._id)){
-              updateLocalWords([...localWords, newLocalWord]);
+    buttons = (
+      <div className="word__interact">
+        <button
+          className="words__interact-hard"
+          onClick={() => {
+            // const newDifficulty = (difficultWord as IUserWord).userWord?.difficulty === 'hard' ? 'easy' : 'hard'
+            // const newIsLearnt = newDifficulty === 'hard' ? false : true;
+            const newLocalWord = createLocalWord(difficultWord);
+            let ids = localWords.map((element) => element._id);
+            if (
+              !ids.includes(newLocalWord._id) ||
+              !areObjectsEqual(
+                newLocalWord.userWord,
+                (difficultWord as IUserWord).userWord!
+              )
+            ) updateLocalWords([...localWords, newLocalWord]);
+            setDifficultWord({
+              ...difficultWord,
+              userWord: {
+                ...(difficultWord as IUserWord).userWord,
+                difficulty: newLocalWord.userWord.difficulty,
+              },
+            });
+            if (numbers.section === 6) {
+              updateWords((wordsArray as IUserWord[]).filter((element) => element._id !== (difficultWord as IUserWord)._id));
             }
-
-            setDifficultWord({ ...difficultWord, userWord: {
-              ...(difficultWord as IUserWord).userWord, difficulty: newDifficulty
-            }});
-            console.log(difficultWord);
-        }}
-      >
-        {(difficultWord as IUserWord).userWord?.difficulty === 'hard' ? 'Убрать из сложных' : 'Отметить как сложное'}
-      </button>
-      <button
-        className='words__interact-learnt'>
-        {(difficultWord as IUserWord).userWord?.optional?.learnt === true ? 'Убрать из изученных' : 'Отметить как изученное'}
-      </button>
-    </div>
+          }}
+        >
+          {(difficultWord as IUserWord).userWord?.difficulty === "hard"
+            ? "Убрать из сложных"
+            : "Отметить как сложное"}
+        </button>
+        <button className="words__interact-learnt">
+          {(difficultWord as IUserWord).userWord?.optional?.learnt === true
+            ? "Убрать из изученных"
+            : "Отметить как изученное"}
+        </button>
+      </div>
+    );
   }
 
   return (
@@ -70,18 +86,28 @@ function Card(props: wordProps) {
           />
         </div>
         <div className="word__meaning">
-          <div className="word__meaning-eng" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(word?.textMeaning as string) }} />
+          <div
+            className="word__meaning-eng"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(word?.textMeaning as string),
+            }}
+          />
           <div className="word__meaning-ru">{word?.textMeaningTranslate}</div>
         </div>
         <div className="word__example">
-          <div className="word__example-eng" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(word?.textExample as string) }} />
+          <div
+            className="word__example-eng"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(word?.textExample as string),
+            }}
+          />
           <div className="word__example-ru">{word?.textExampleTranslate}</div>
         </div>
         {buttons}
       </div>
       <img className="word__image" src={link} alt={word?.word} />
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
