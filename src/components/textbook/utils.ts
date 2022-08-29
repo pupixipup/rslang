209b73a4cdef3471abe7c31ab72f3c3af106ea03
pupixipup <rkbitinas@gmail.com>
2022-08-29 11:57:06 +0200

    refactor: apply prettier to utils

--- src/components/textbook/utils.ts
+++ src/components/textbook/utils.ts
@@ -1,28 +1,36 @@
-import { localWord, IUserWord } from "../../common/interfaces";
+import { localWord, IUserWord, IWord } from "../../common/interfaces";
 
 export class wordUtils {
   static areObjectsEqual(obj1: object, obj2: object) {
-   return JSON.stringify(obj1) === JSON.stringify(obj2)
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
   }
+
   static countHardWords(array: IUserWord[]) {
-    return array.filter((element) => element.userWord?.difficulty === 'hard').length;
+    return array.filter((element) => element.userWord?.difficulty === "hard")
+      .length;
   }
+
   static countLearntWords(array: IUserWord[]) {
     return array.filter((element) => {
       if (element.userWord?.optional?.learnt === undefined) return false;
-      return element.userWord?.optional?.learnt}).length;
+      return element.userWord?.optional?.learnt;
+    }).length;
   }
 
   static getIdsArray(array: localWord[]) {
     return array.map((element) => element._id);
   }
-  
+
   static getUniqueWords(array1: localWord[], array2: IUserWord[]) {
     const idsOnly = wordUtils.getIdsArray(array1);
-    return array2.filter(element => !idsOnly.includes(element._id));
+    return array2.filter((element) => !idsOnly.includes(element._id));
   }
-  static updateLocalWord(array1: localWord[], newWord: localWord, update: (arr: localWord[]) => void) {
-    // array1 - localwords, newWord
+
+  static updateLocalWord(
+    array1: localWord[],
+    newWord: localWord,
+    update: (arr: localWord[]) => void
+  ) {
     const ids = wordUtils.getIdsArray(array1);
     if (ids.includes(newWord._id)) {
       const filteredLocalWords = array1.filter(
@@ -33,4 +41,45 @@ export class wordUtils {
       update([...array1, newWord]);
     }
   }
+
+  static createLocalisLearntWord(difficultWord: IUserWord | IWord) {
+    const oldIsLearnt = (difficultWord as IUserWord).userWord?.optional?.learnt;
+    const newDifficulty = !oldIsLearnt
+      ? "easy"
+      : (difficultWord as IUserWord).userWord?.difficulty!;
+    const newIsLearnt = !oldIsLearnt;
+
+    return {
+      _id: (difficultWord as IUserWord)._id,
+      userWord: {
+        difficulty: newDifficulty,
+        optional: {
+          ...(difficultWord as IUserWord).userWord?.optional,
+          learnt: newIsLearnt,
+        },
+      },
+      isUserWord: !!(difficultWord as IUserWord).userWord,
+    };
+  }
+
+  static createLocalDifficultyWord(difficultWord: IUserWord | IWord) {
+    const oldIsLearnt = (difficultWord as IUserWord).userWord?.optional?.learnt;
+    const newDifficulty =
+      (difficultWord as IUserWord).userWord?.difficulty === "hard"
+        ? "easy"
+        : "hard";
+    const newIsLearnt = newDifficulty === "hard" ? false : oldIsLearnt;
+    return {
+      _id: (difficultWord as IUserWord)._id,
+      userWord: {
+        difficulty: newDifficulty,
+        optional: {
+          ...(difficultWord as IUserWord).userWord?.optional,
+          learnt: newIsLearnt,
+        },
+      },
+      isUserWord: !!(difficultWord as IUserWord).userWord,
+    };
+  }
+  
 }
