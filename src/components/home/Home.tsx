429ea7d905eefebf9fcbdbf0377061658cfceb71
vpuzyrevich <v.puzyrevich@gmail.com>
2022-09-04 21:21:00 +0300

    feat: add responsive homepage

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../footer/Footer";
 import './Home.scss';
 
 class Home extends React.Component{
@@ -6,12 +7,15 @@ class Home extends React.Component{
     return (
       <React.StrictMode>
         <div className="home">
-          <div className="home-content">
-            <h1 className="home-title">RSLang</h1>
-            <div className="home-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum rerum deleniti optio quos voluptatibus quis eius.</div>
-            <button className="home-btn">О команде</button>
+          <div className="home-wrapper">
+            <div className="home-content">
+              <h1 className="home-title">RSLang</h1>
+              <div className="home-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum rerum deleniti optio quos voluptatibus quis eius.</div>
+              <button className="home-btn">О команде</button>
+            </div>
+            <div className="home-image"></div>
           </div>
-          <div className="home-image"></div>
+          <Footer />
         </div>
       </React.StrictMode>
     );
