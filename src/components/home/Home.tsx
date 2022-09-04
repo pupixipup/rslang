c6af52b29018f76dbbade467b4b18a71a40e1730
vpuzyrevich <v.puzyrevich@gmail.com>
2022-09-04 18:58:10 +0300

    feat: add basic home page layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import './Home.scss';
 
 class Home extends React.Component{
   render(): React.ReactNode {
     return (
       <React.StrictMode>
-        <p>HOME</p>
+        <div className="home">
+          <div className="home-content">
+            <h1 className="home-title">RSLang</h1>
+            <div className="home-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum rerum deleniti optio quos voluptatibus quis eius.</div>
+            <button className="home-btn">О команде</button>
+          </div>
+          <div className="home-image"></div>
+        </div>
       </React.StrictMode>
     );
   }    
