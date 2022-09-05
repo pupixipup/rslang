19165b1787d9e6555ce4c7686171a7501b5d9980
vpuzyrevich <v.puzyrevich@gmail.com>
2022-09-05 10:25:16 +0300

    feat: add description to homepage

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -13,6 +13,7 @@ export function Home () {
           <div className="home-content">
             <h1 className="home-title"><span>RS</span><span>L</span><span>a</span><span>n</span><span>g</span></h1>
             <div className="home-description">Повторение - мать учения. Наше приложение превратит изучение английских слов в увлекательный процесс.</div>
+            <div className="home-description">Знакомьтесь со словами в Словаре и закрепляйте знания в Играх. Авторизуйтесь, и вы сможете создавать списки сложных слов, вести статистику изучения слов за день и за все время обучения.</div>
             <button className="home-btn" onClick={() => navigate(MENUITEMS[9].link)}>О команде</button>
           </div>
           <div className="home-image"></div>
