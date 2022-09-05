7c38fbfa9ee564afbe25bfea8508b70812186678
vpuzyrevich <v.puzyrevich@gmail.com>
2022-09-05 13:12:23 +0300

    feat: add avatars, fix adaptation

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,7 +3,6 @@ import "./about.scss";
 
 export default function About() {
     return (
-        <div>
             <div className="about">
                 
                 <div className="about__wrapper">
@@ -16,35 +15,42 @@ export default function About() {
 
                     <div className="team">
                         <div className="team__card">
-                            <h3>Виктория</h3>
-                            <p>разработала компоненты авторизации и игру "Спринт"</p>
-                            <a className="team__git-nickname"
-                                href="https://github.com/vpuzyrevich"
-                                target="_blank"
-                                rel="noopener noreferrer">vpuzyrevich</a>
+                            <div className="team__avatar avatar-v"></div>
+                            <div className="team__content">
+                                <h3>Виктория</h3>
+                                <p>разработала компоненты авторизации, главную страницу и игру "Спринт"</p>
+                                <a className="team__git-nickname"
+                                    href="https://github.com/vpuzyrevich"
+                                    target="_blank"
+                                    rel="noopener noreferrer">vpuzyrevich</a>
+                            </div>
                         </div>
                         <div className="team__card">
-                            <h3>Наталья</h3>
-                            <p>разработала компоненты статистики, главное меню</p>
-                            <a className="team__git-nickname"
-                                href="https://github.com/BlueOwll"
-                                target="_blank"
-                                rel="noopener noreferrer">BlueOwll</a>
+                            <div className="team__avatar avatar-n"></div>
+                            <div className="team__content">
+                                <h3>Наталья</h3>
+                                <p>разработала компоненты статистики, главное меню, API приложения, страницу "О команде"</p>
+                                <a className="team__git-nickname"
+                                    href="https://github.com/BlueOwll"
+                                    target="_blank"
+                                    rel="noopener noreferrer">BlueOwll</a>
+                            </div>
                         </div>
                         <div className="team__card">
-                            <h3>Ромас</h3>
-                            <p>разработал словарь и игру "Аудиовызов"</p>
-                            <a className="team__git-nickname"
-                                href="https://github.com/pupixipup"
-                                target="_blank"
-                                rel="noopener noreferrer">pupixipup</a>
+                            <div className="team__avatar avatar-r"></div>
+                            <div className="team__content">
+                                <h3>Ромас</h3>
+                                <p>разработал словарь и игру "Аудиовызов"</p>
+                                <a className="team__git-nickname"
+                                    href="https://github.com/pupixipup"
+                                    target="_blank"
+                                    rel="noopener noreferrer">pupixipup</a>
+                            </div>
                         </div>
-
                     </div>
                     </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
     );
 }
